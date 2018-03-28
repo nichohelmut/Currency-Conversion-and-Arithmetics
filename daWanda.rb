03957de20a3a1d37f665e0e69c17f52346b17b8f
Nicholas Utikal <nicholasutikal@gmail.com>
2018-03-28 19:06:03 +0200

    working on the == conversion

--- daWanda.rb
+++ daWanda.rb
@@ -8,7 +8,7 @@ class Money
 
   def self.conversion_rates(attr = {})
     # @conv_rates = {from_curr = {rates}} Use Hard Code for now
-    # refactor to arrt ={}
+    # refactor to attr ={}
     return {'EUR'=>{'USD'=>1.2, 'Bitcoin'=>0.0047},
     'USD'=>{'EUR'=>0.8, 'Bitcoin'=>0.0097},
     'Bitcoin'=>{'USD'=>1.11, 'EUR'=>0.0047}}
@@ -30,8 +30,9 @@ class Money
     if to_curr != self.from_curr
       @rate = Money.conversion_rates[self.from_curr]["#{to_curr}"]
       @converted_amount = @amount * @rate
+      Money.new(@converted_amount, to_curr)
     else
-      @amount
+      Money.new(@amount, from_curr)
     end
   end
 # --------------------------------------
@@ -49,16 +50,17 @@ class Money
     self.amount * other
   end
 
-  def /(other)
+  def /(other)twe
     self.amount / other
   end
 
   def ==(other)
     curr = self.from_curr
+    puts other
     self.amount == other.convert_to(curr)
   end
 end
 
 twenty_dollars = Money.new(20, "USD")
-  # = a.convert_to("EUR")
+# twenty_dollars  = a.convert_to("EUR")
 fifty_euro = Money.new(50, "EUR")
